aac02bfbc5a2643a6b0e6b482d139bae9619a81f
Sergiu Nacu <snacu@salesforce.com>
2025-10-31 16:38:45 -0700

    fix BASE_URL

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,9 @@
 import db from '$lib/db.js'
 
 let baseUrl = 'http://localhost:5173'
-const definedBaseUrl = !!process.env.PUBLIC_BASE_URL
+const definedBaseUrl = !!process.env.BASE_URL
 if (definedBaseUrl) {
-  baseUrl = `https://${process.env.PUBLIC_BASE_URL.replace('https://', '')}`
+  baseUrl = `https://${process.env.BASE_URL.replace('https://', '')}`
 }
 
 /** @type {import('./$types').PageServerLoad} */
