8cd54c95f5c7242b9c4140b3414681f71034ab07
Sergiu Nacu <snacu@salesforce.com>
2025-10-31 13:25:51 -0700

    initial commit

---
+++ src/routes/info/+server.js
@@ -0,0 +1,15 @@
+import { json } from '@sveltejs/kit';
+import pkg from '../../../package.json' assert { type: 'json' };
+
+// Track server start time
+const START_TIME = Date.now();
+
+export function GET() {
+	const upSince = Math.floor((Date.now() - START_TIME) / 1000);
+
+	return json({
+		seconds_since_start: upSince,
+		version: pkg.version
+	});
+}
+
